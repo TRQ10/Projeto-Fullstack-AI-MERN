5f3fc411d3af7e88cd2d7e566372f876869fa812
Camila Oliveira <114073795+Cxmxla@users.noreply.github.com>
2023-03-06 00:01:28 -0300

    mudanças no form-field

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -10,11 +10,11 @@ const FormField = ({
   isSurpriseMe,
   handleSurpriseMe,
 }) => (
-  <div>
-    <div className="flex items-center gap-2 mb-2">
+  <div className='w-full'>
+    <div className="mb-2">
       <label
         htmlFor={name}
-        className="block text-sm font-medium text-gray-900"
+        className="text-s font-medium text-white text-left"
       >
         {labelName}
       </label>
@@ -32,7 +32,8 @@ const FormField = ({
       type={type}
       id={name}
       name={name}
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
+      className="bg-gray-50 border border-[#9d2053] text-gray-900 text-sm rounded-xl 
+      focus:ring-[#6469ff] focus:border-[#6469ff] outline-none p-3 w-full"
       placeholder={placeholder}
       value={value}
       onChange={handleChange}
