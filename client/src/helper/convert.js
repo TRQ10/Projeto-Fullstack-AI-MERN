330fb43330e69b89c6e0157785b1d29e58a135a1
Dayse Campos <111383784+dayseCampos@users.noreply.github.com>
2023-03-06 08:46:44 -0300

    correção

--- client/src/helper/convert.js
+++ client/src/helper/convert.js
@@ -1,4 +1,4 @@
-/** image onto base64 */
+/** imagem vira base64 */
 export default function convertToBase64(file){
     return new Promise((resolve, reject) => {
         const fileReader = new FileReader();
