69529761c0469955d950b3581eb91f2453df1554
Tiago Roque <110204165+TRQ10@users.noreply.github.com>
2023-03-18 16:58:51 -0300

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('https:localhost:3000/api/v1/post', {
+        const response = await fetch('http:localhost:3000/api/v1/post', {
           methods: 'GET',
           headers: {
             'Content-Type': 'application/json',
